f221c22291746ebe7ee8be581fbc42f65ccf2e25
Swapnil <68826225+thespcrewroy@users.noreply.github.com>
2024-02-08 06:24:33 -0500

    Update SFImage.swift

--- MyFirstContentView/MyFirstContentView/SFImage.swift
+++ MyFirstContentView/MyFirstContentView/SFImage.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SFImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+         Image(systemName: "trash", variableValue: 0.6)
+            .font(.system(size: 100))
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.indigo)
+            .font(.largeTitle)
     }
 }
 
