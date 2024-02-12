483f3245434481bb25049ba76eeb29ffe509ccb0
Swapnil <68826225+thespcrewroy@users.noreply.github.com>
2024-02-12 15:19:59 -0500

    Add Assignnment 4

---
+++ Homework/Assignment_4/Assignment_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_4App.swift
+//  Assignment_4
+//
+//  Created by NMI Student on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
