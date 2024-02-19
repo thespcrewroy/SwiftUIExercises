a19b428815f4aa3f22d71890f2a64ddc49a3414c
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-02-19 14:46:27 -0500

    Added SwiftUIState

---
+++ Practice/SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by Swapnil Roy on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
