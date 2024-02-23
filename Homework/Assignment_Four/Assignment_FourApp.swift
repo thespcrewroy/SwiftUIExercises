481ae4ba08266852d2747a20a60697b5b172c2ac
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-02-22 22:48:47 -0500

    Added Assignment_4

---
+++ Homework/Assignment_Four/Assignment_FourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_FourApp.swift
+//  Assignment_Four
+//
+//  Created by Swapnil Roy on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_FourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
