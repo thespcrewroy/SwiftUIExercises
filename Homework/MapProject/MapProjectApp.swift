f201e72878ce33c01423c357737fd9c07ebda891
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-05-01 12:34:31 -0400

    New hw10-maps

---
+++ Homework/MapProject/MapProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapProjectApp.swift
+//  MapProject
+//
+//  Created by Swapnil Roy on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MapProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
