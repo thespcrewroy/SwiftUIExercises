b422cbf5ce69f63a2a7ec364ddf992528e2b6f20
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-08-18 18:50:44 -0400

    New Random Trivia

---
+++ Projects/02. RandomtTrivia/RandomTriviaAPI/RandomTriviaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTriviaAPIApp.swift
+//  RandomTriviaAPI
+//
+//  Created by Swapnil Roy on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomTriviaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
